cbc3792471d03705f524a4c2e3d4e02785c6ff35
ashmuradyann <ashot.muradyan16@gmail.com>
2022-08-02 00:15:28 +0300

    adding categories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 
-import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
 import Authentication from './routes/authentication/authentication.component'
 import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
@@ -11,15 +11,15 @@ const App = () => {
 
   const navigate = useNavigate()
 
-  useEffect(() => {
-    navigate("/")
-  }, [])
+  // useEffect(() => {
+  //   navigate("/")
+  // }, [])
 
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
       </Route>
@@ -28,4 +28,4 @@ const App = () => {
 
 }
 
-export default App;
+export default App
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,16 @@ import {
     signOut,
     onAuthStateChanged
 } from 'firebase/auth'
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs 
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyC5YM0vfFrFf53P1zHDLaY2hR7IUM8l9NQ",
@@ -34,9 +43,36 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore()
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey)
+    const batch = writeBatch(db)
+
+    objectsToAdd.forEach(object => {
+        const docRef = doc(collectionRef, object.title.toLowerCase())
+        batch.set(docRef, object)
+    })
+
+    await batch.commit()
+    console.log("done")
+}
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories')
+    const q  = query(collectionRef)
+
+    const querySnapshot = await getDocs(q)
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data()
+        acc[title.toLowerCase()] = items
+        return acc
+    }, {})
+
+    return categoryMap
+}
+
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
-    if(!userAuth) return
-    
+    if (!userAuth) return
+
     const userDocRef = doc(db, "users", userAuth.uid)
 
     const userSnapshot = await getDoc(userDocRef)
@@ -56,18 +92,18 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
             console.log('error creating the user', error.message)
         }
     }
-    
+
     return userDocRef
 }
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-    if(!email || !password) return
+    if (!email || !password) return
 
     return await createUserWithEmailAndPassword(auth, email, password)
 }
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-    if(!email || !password) return
+    if (!email || !password) return
 
     return await signInWithEmailAndPassword(auth, email, password)
 }
